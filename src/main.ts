982d18337172f2a09705fcca629e9533d7e4661c
Ikramullah <ikramullahzx@gmail.com>
2021-02-27 11:05:14 +0800

    Reinit with unix system

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,36 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { WAConnection } from '@adiwajshing/baileys'
+import * as FS from 'fs'
+
+const client = new WAConnection()
+
+async function connectToWhatsApp () {
+  // Load credentials if available
+  if (FS.existsSync('./authentication.json')) {
+    client.loadAuthInfo('./authentication.json')
+  }
+
+  // Save credentials if updated
+  client.on('credentials-updated', () => {
+    console.log("Credentials updated!")
+    const authinfo = client.base64EncodedAuthInfo()
+    FS.writeFileSync('./authentication.json', JSON.stringify(authinfo, null, '\t'))
+  })
+
+  // Awaiting connect flag
+  await client.connect()
+
+  // Handling chat
+  client.on('chat-update', (chat) => {
+    if (!chat.messages && !chat.count) return
+    
+    console.log('events::chat_update')
+    const message = chat.messages.all()
+    console.log(message)
+  })
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+connectToWhatsApp()
+  .catch(err => {
+    throw new Error(err)
+  })
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
